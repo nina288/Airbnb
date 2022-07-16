423400e17aaab9eaa8ec704c1f2540149e8cbaf8
nina288 <75220466+nina288@users.noreply.github.com>
2022-07-16 21:15:04 +0300

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img src="./images/photo-grid.png" className="hero--photo" alt="" />
+      <h1 className="hero--header">Опыт работы online</h1>
+      <p className="hero--text">
+        Присоединяйтесь к уникальным интерактивным мероприятиям, проводимым
+        единственными в своем роде ведущими - и все это, не выходя из дома.
+      </p>
+    </section>
+  );
+}
